e973b260834fa634f5e26731b06e0b55e3ff4d53
juanpabloleonpersonals <juleon@personalsoft.com.co>
2019-08-14 22:51:03 -0500

    Comit inicial para ramas de proyecto de prueba

---
+++ src/main/java/ConceptosDos.java
@@ -0,0 +1,15 @@
+public class ConceptosDos {
+
+    public PruebaConceptos pruebaConceptos;
+
+    public ConceptosDos() {
+
+        pruebaConceptos= new PruebaConceptos();
+
+    }
+
+    public void metodo_sin_retorno(){
+        pruebaConceptos.metodo_sin_retorno(2,"cadena");
+        pruebaConceptos.metodo_prueba();
+     }
+}
---
+++ src/main/java/PruebaConceptos.java
@@ -0,0 +1,43 @@
+public class PruebaConceptos {
+
+    // comentario de línea
+    /**
+    Comentario de bloque
+     **/
+    // variaable tipo double
+    public double pruebaDouble;
+    // variable tipo cadena global
+    public String pruebaCadena;
+
+    public PruebaConceptos(){
+
+
+    }
+
+
+    /**
+     *
+     * @return
+     */
+   public double metodo_prueba(){
+     // variable tipo double local
+       double variable=0;
+
+        return variable;
+    }
+
+    /**
+     *
+     * @param var1
+     * @param cad1
+     *
+     */
+    public void metodo_sin_retorno(int var1, String cad1){
+
+    }
+
+
+
+
+
+}
